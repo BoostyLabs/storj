f67c622a202f783d05291ea3ce5101e63f30345f
Dmytro Yakimuk <dmytroyakimuk@gmail.com>
2022-01-18 20:00:15 +0200

    added download and upload methods

---
+++ remotefilestorage.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2021 Creditor Corp. Group.
+// See LICENSE for copying information.
+
+package storj
+
+import (
+	"context"
+)
+
+// RemoteFileStorage interface to call s3 with minio.
+type RemoteFileStorage interface {
+	Upload(ctx context.Context, bucket, objectName string, data []byte) error
+	Download(ctx context.Context, bucket, objectName string) ([]byte, error)
+}
